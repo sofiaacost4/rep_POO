0304f629616a4f7530a214f9e7851e74fea45862
sofiaacost4 <k.sofia@escolar.ifrn.edu.br>
2025-07-10 13:18:58 +0000

    lista 5 boleto e paciente

--- aula_13/lista_05.py
+++ aula_13/lista_05.py
@@ -1,65 +1,31 @@
 from datetime import datetime
-class Paciente:
-    def __init__(self, no, c, t, na):
-        self.__nome = no
-        self.__cpf = c
-        self.__telefone = t
-        self.__nascim = na
-    def get_nome(self):
-        return self.__nome
-    def get_cpf(self):
-        return self.__cpf
-    def get_nascim(self):
-        return self.__nascim
+from enum import Enum
+class Pagamento(Enum):
+    EmAberto = 0
+    PagoParcial = 1
+    Pago = 2
+class Boleto:
+    def __init__(self, c, dE, dV, dP, vB, vP):
+        self.__codigo = c
+        self.__dtEmissao = dE
+        self.__dtVencimento = dV
+        self.__dtPago = dP
+        self.__vBoleto = vB
+        self.__vPago = 0
+        self.__situacao = Pagamento.EmAberto
+    def pagar(self, valorpago):
+        if valorpago == self.__vBoleto:
+            return Pagamento.Pago
+        if valorpago != 0 and valorpago < self.__vBoleto:
+            return Pagamento.PagoParcial
+    def situacao(self):
+        return self.__situacao
+
+
     def __str__(self):
-        return f"Nome: {self.__nome} - Cpf: {self.__cpf} - Telefone: {self.__telefone} - Data de Nascimento: {self.__nascim}"
-    
-class PacienteUI:
-    __pacientes = []
-    @classmethod
-    def main(cls):
-           op = 0
-           while op != 6:
-            op = PacienteUI.menu()
-            if op == 1: PacienteUI.inserir()
-            if op == 2: PacienteUI.idade()
-            if op == 3: PacienteUI.alterar()
-            if op == 4: PacienteUI.recuperar()
-            if op == 5: PacienteUI.listar()
+        return f"Código de barras: {self.__codigo} - Data de emissão: {self.__dtEmissao} - Data de vencimento: {self.__dtVencimento} - Data de pagamento: {self.__dtPago} - Valor do boleto: {self.__vBoleto} - Valor pago: {self.__vPago} - Situação do pagamento: {Boleto.situacao()}"
+
+class BoletoUI:
     @classmethod
     def menu(cls):
-        print("1 - Inserir dados | 2 - Idade do paciente | 3 - Alterar dados | 4 - Recuperar dados | 5 - Listar | 6 - Fim")
-        return int(input("Escolha uma opção: "))
-    @classmethod
-    def inserir(cls):
-        nome = input("Nome do paciente: ")
-        cpf = (input("Cpf do paciente: "))
-        telefone = (input("Telefone do paciente: "))
-        nascimento = input("Data de nascimento do paciente: ")
-        dn = datetime.strptime(nascimento, "%d/%m/%Y")
-        P = Paciente(nome, cpf, telefone, dn)
-        cls.__pacientes.append(P)
-    @classmethod
-    def listar(cls):
-        for p in cls.__pacientes:
-            print(p)
-    @classmethod
-    def idade(cls):
-        cpf = input("Qual é o cpf da pessoa que deseja saber a idade? ")
-        for c in cls.__pacientes:
-            if c.get_cpf() == cpf:
-                print(cpf)
-                hj = datetime.now()
-                i = hj - c.get_nascim()
-                anos = (i.days // 365)
-                meses = (i.days % 365 // 30)
-                print(f"Idade de {c.get_nome()}: {anos} anos e {meses} meses.")
-                return
-            print("Cpf não encontrado.")
-    @classmethod
-    def alterar(cls):
-        pass
-    @classmethod
-    def recuperar(cls):
-        pass
-PacienteUI.main()
+        op = int(input("1 - Inserir dados | 2 - Situação | 3 - Pagar | 4 - Alterar | 5 - Recuperar | 6 - Fim"))
