f1efcce4a24d297125c093c83713a5acf3383bbb
sofiaacost4 <k.sofia@escolar.ifrn.edu.br>
2025-11-05 18:49:05 +0000

    lista 14 commit final

--- Agendamento/templates/manterhorarioUI.py
+++ Agendamento/templates/manterhorarioUI.py
@@ -22,10 +22,13 @@ class ManterHorarioUI:
                 cliente = View.cliente_listar_id(obj.get_id_cliente())
                 servico = View.servico_listar_id(obj.get_id_servico())
                 profissional = View.profissional_listar_id(obj.get_id_profissional())
+                estado_pagamento = View.pagamento_listar_por_horario(obj.get_id())
                 if cliente != None: cliente = cliente.get_nome()
                 if servico != None: servico = servico.get_descricao()
                 if profissional != None: profissional = profissional.get_nome()
-                dic.append({"id" : obj.get_id(), "data" : obj.get_data(), "confirmado" : obj.get_confirmado(), "cliente" : cliente, "serviço" : servico, "profissional": profissional})
+                if estado_pagamento != None: estado_pagamento = estado_pagamento.get_estado()
+                else: estado_pagamento = "Pendente"
+                dic.append({"id" : obj.get_id(), "data" : obj.get_data(), "confirmado" : obj.get_confirmado(), "cliente" : cliente, "serviço" : servico, "pagamento" : estado_pagamento, "profissional": profissional})
             df = pd.DataFrame(dic)
             st.dataframe(df)
         return horarios
--- Agendamento/templates/visualizaragendaUI.py
+++ Agendamento/templates/visualizaragendaUI.py
@@ -11,12 +11,15 @@ class VisualizarAgendaUI:
             cliente = View.cliente_listar_id(obj.get_id_cliente())
             servico = View.servico_listar_id(obj.get_id_servico())
             profissional = View.profissional_listar_id(obj.get_id_profissional())
+            estado_pagamento = View.pagamento_listar_por_horario(obj.get_id())
             if cliente != None: cliente = cliente.get_nome()
             if servico != None: servico = servico.get_descricao()
             if profissional != None: profissional = profissional.get_nome()
+            if estado_pagamento != None: estado_pagamento = estado_pagamento.get_estado()
+            else: estado_pagamento = "Pendente"
             if obj.get_id_profissional() == (st.session_state["usuario_id"]):
 
-                dic.append({"id" : obj.get_id(), "data" : obj.get_data(), "confirmado" : obj.get_confirmado(), "cliente" : cliente, "serviço" : servico})
+                dic.append({"id" : obj.get_id(), "data" : obj.get_data(), "confirmado" : obj.get_confirmado(), "cliente" : cliente, "serviço" : servico, "pagamento" : estado_pagamento})
         if dic:
             df = pd.DataFrame(dic)
             st.dataframe(df)
